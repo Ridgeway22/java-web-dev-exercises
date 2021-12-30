56aea038177b88b2891915dd644f2afd79cc1265
Ridgeway22 <boo22mickey@yahoo.com>
2021-12-30 13:50:56 -0600

    git commit -m"done with studio excluding bonus"

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,15 @@
+package org.launchcode.java.studios.areaofacircle;
+import org.launchcode.java.Circle;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius");
+        double radius = input.nextDouble();
+        double area1 = Circle.getArea(radius);
+        System.out.println("The area of a circle of radius "+ radius + " is: " + area1);
+        input.close();
+    }
+}
